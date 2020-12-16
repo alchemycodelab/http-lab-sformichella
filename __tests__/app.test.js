35e608f2a2b6c5e8968c4b7dff55f38969fd4f41
sformichella <sformichella@gmail.com>
2020-12-15 16:40:40 -0800

    get / route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('returns plaintext hi', async() => {
+    const response = await request(app)
+      .get('/');
 
+    expect(response.body).toEqual('hi');
+  });
 });
